7925443932a71b51919a1047bdb17d8ef70c98f7
Mona Lisa <email@example.com>
2018-11-25 12:08:37 +0100

    Mobile ended

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 //hamburger button
 let hamburger = document.querySelector (".site-nav--toggle");
 hamburger.addEventListener("click", function() {
-  document.querySelector(".header").classList.toggle("is-expanded");
+  document.querySelector(".site-nav__container").classList.toggle("is-expanded");
 }, false)
 
-//carousel
+// show/hide contact
+let contactShow = document.querySelector (".contact__btn--show");
+contactShow.addEventListener("click", function() {
+  document.querySelector(".contact-container").classList.add("is-visible");
+}, false);
+
+let contactClose = document.querySelector (".contact__btn--close");
+contactClose.addEventListener("click", function() {
+  document.querySelector(".contact-container").classList.remove("is-visible");
+}, false)
