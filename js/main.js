df54a8ff61557120a8fac2f0e13a0f109d9473c0
jaro <jarosz.bartlomiej89@gmail.com>
2019-03-28 09:38:33 +0100

    Main.js problem

--- js/main.js
+++ js/main.js
@@ -1,64 +1,44 @@
 //hamburger button
-let navFunctions = function (event) {
+const navFunctions = () => {
   document.querySelector(".site-nav__container").classList.toggle("is-expanded");
   document.body.classList.toggle("nav-opened");
 };
-let hamburger = document.querySelector (".site-nav--toggle");
-hamburger.addEventListener("click", navFunctions, false);
+const hamburger = document.querySelector (".site-nav--toggle");
+hamburger.addEventListener("click", navFunctions);
 
-let navLink = document.querySelectorAll (".site-nav__item");
-  for (let i=0; i < navLink.length; i++) {
-    navLink[i].addEventListener("click", navFunctions, false);
+const navItems = document.querySelectorAll (".site-nav__item");
+  for (let i=0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", navFunctions);
   };
 
-
-// show/hide contact
-let contactShow = document.querySelector (".contact__btn--show");
-contactShow.addEventListener("click", function() {
-  document.querySelector(".contact-container").classList.add("is-visible");
-}, false);
-
-let contactClose = document.querySelector (".contact__btn--close");
-contactClose.addEventListener("click", function() {
-  document.querySelector(".contact-container").classList.remove("is-visible");
-}, false);
-
-
-//Desktop mediaquery
 //logo scroll animation
-
+  //Desktop mediaquery
 if (matchMedia) {
   const desktop = window.matchMedia("(min-width: 1200px)");
-  desktop.addListener(DesktopChange);
-  DesktopChange(desktop);
+  desktop.addListener(desktopChange);
+  desktopChange(desktop);
 }
-function DesktopChange(desktop) {
-  let logoAnimation = document.querySelector (".banner__animation"),
-      banner = document.querySelector (".banner"),
-      scrollTop = 0,
-      frame = 0,
-      framesTotal = 57;
-if (desktop.matches) {
-  document.addEventListener("scroll", function() {
-    scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    frame = parseInt(scrollTop/10);
-    if (frame >= 0 && frame < framesTotal) {
-      logoAnimation.style.backgroundPosition = "center -" + frame*481 + "px"
-
-    } if (frame >= 50) {
-      banner.style.position = "relative",
-      banner.style.top = "500px"
-    } else {
-      banner.style.position = "sticky",
-      banner.style.top = "0"
-    }
-  })
-}
+function desktopChange(desktop) {
+  const logoAnimation = document.querySelector (".banner__animation"),
+        banner = document.querySelector (".banner");
+    let scrollTop = 0,
+        frame = 0,
+        framesTotal = 57;
+  if (desktop.matches) {
+    document.addEventListener("scroll", () => {
+      scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+      frame = parseInt(scrollTop/10);
+      if (frame >= 0 && frame < framesTotal) {
+        logoAnimation.style.backgroundPosition = `center -${frame*481}px`;
+        (frame >= 50) ? banner.classList.add("offset") : banner.classList.remove("offset");
+      }
+    })
+  }
 };
 //trigger animations
-let animationTrigger = function () {
-  let animation;
-  let windowHeight;
+const animationTrigger = () => {
+  let animation,
+      windowHeight;
   function init() {
     animation = document.querySelectorAll (".not-active");
     windowHeight = window.innerHeight;
@@ -73,12 +53,8 @@ let animationTrigger = function () {
     for (let i = 0; i < animation.length; i++) {
       let positionFromTop = animation[i].getBoundingClientRect().bottom;
       if (positionFromTop - windowHeight <= 20) {
-        animation[i].className = animation[i].className.replace (
-          "not-active",
-          "is-active"
-        );
+        animation[i].className = animation[i].className.replace ("not-active", "is-active");
       }
-
     }
   }
   return {
@@ -86,3 +62,14 @@ let animationTrigger = function () {
   };
 };
 animationTrigger().init();
+
+// show/hide contact
+const contactShow = document.querySelector (".contact__btn--show");
+contactShow.addEventListener("click", () => {
+  document.querySelector(".contact-container").classList.add("is-visible");
+});
+
+const contactClose = document.querySelector (".contact__btn--close");
+contactClose.addEventListener("click", function() {
+  document.querySelector(".contact-container").classList.remove("is-visible");
+});
